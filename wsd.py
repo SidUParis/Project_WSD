94276f3ed60c96adc15c63f2aaddd7d26aaae5fe
xsun <sunxusidney@gmail.com>
2024-04-30 17:39:09 +0000

    final version

--- wsd.py
+++ wsd.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+
+### REF
+
+# 1. https://stackoverflow.com/questions/34735016/choosing-an-sklearn-pipeline-for-classifying-user-text-data 
+# 2. ChatGPT
+# 3. https://scikit-learn.org/stable/modules/naive_bayes.html 
+
 from collections import defaultdict
 from nltk.corpus import wordnet # This might require "nltk.download('wordnet')" and "nltk.download('omw-1.4')".
 import math
+
 import random
 from sklearn.model_selection import KFold
 
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import make_pipeline
+import pathlib
 
 from utils import *
 
@@ -75,16 +84,21 @@ class MostFrequentSense(WSDClassifier):
         """
         instances: list[WSDInstance]
         """
+
         senDISTR = sense_distribution(instances) # dict[string -> int]
+        
+        # print("first get :",senDISTR) # initialized 
         for instance in instances:
+            # print(f"instance: {instance},instance.lemma:{instance.lemma}")
             if instance.lemma not in self.mfs:
                 self.mfs[instance.lemma] = instance.sense
+                
             else:
                 if senDISTR[instance.sense] > senDISTR[self.mfs[instance.lemma]]:
                     self.mfs[instance.lemma] = instance.sense
-        # by this we are meaning that the most frequent sense is the one that appears the most in the training data, so we are going to use the sense that appears the most in the training data as the most frequent sense for the lemma
-        #这个训练时代表的是，对于每个lemma，我们找到了在训练数据中出现最多的sense，然后将其作为这个lemma的最频繁sense，也就是说，我们认为这个sense是这个lemma的最频繁sense，然后在预测时，我们就直接返回这个sense
-        
+
+        # print("most frequent:",self.mfs[instance.lemma]) # get the most frequent sense for the lemma
+        print("mfs:",self.mfs)
         pass # TODO
 
     def predict_sense(self, instance):
@@ -116,46 +130,45 @@ class SimplifiedLesk(WSDClassifier):
         plus the word itself.
         """
         for lemma in WN_CORRESPONDANCES:
-            # print(lemma)  ##uncomment for debug ----sun
+         
             for sense in WN_CORRESPONDANCES[lemma]:
-                # print(sense) ##uncomment for debug ----sun
+
                 self.signature[sense] = set() # initialize the signature of the sense 
-                # DEFINITION USING UPDATE INSTEAD OF ADD BECAUSE WE WANT TO ADD MULTIPLE ELEMENTS --sun
+
                 for synset in WN_CORRESPONDANCES[lemma][sense]:
                     self.signature[sense].update(self.wn_synet(synset).definition().split())
-                # print(signatures[sense]) ##uncomment for debug ----sun
-                # EXAMPLE USING UPDATE INSTEAD OF ADD BECAUSE WE WANT TO ADD MULTIPLE ELEMENTS --sun
+
                 for example in self.wn_synet(synset).examples():
                     self.signature[sense].update(example.split())
-                # ADD LEMMA by using add instead of update because we want to add only one element --- sun
+
                     self.signature[sense].add(lemma) # because update is to use for multiple 
-                # print(self.signature[sense]) ##uncomment for debug ----sun
+
         ### context 
         for instance in instances:
             if self.window_size == -1:
                 self.signature[instance.sense].update(instance.context)
             else:
                 self.signature[instance.sense].update(instance.left_context[-self.window_size:]+instance.right_context[:self.window_size])
-            # print(self.signature) ##uncomment for debug ----sun
 
      
   
     def predict_sense(self, instance):
         ## 2 different ways to get the prediction, one using window size and the other without window size
-        
-        all_senses = list(WN_CORRESPONDANCES[instance.lemma].keys()) # list[string]
-        
-        # mfs = all_senses[0]  # Assume the first sense in the list is the most frequent
+        # all_senses = list(WN_CORRESPONDANCES[instance.lemma].keys()) # list[string]
+        senses = list(WN_CORRESPONDANCES[instance.lemma].keys()) # list[string]
+
         scores = list(len(senses)*[0]) # initialize the scores for each sense
         for i,sense in enumerate(senses):
             for word in instance.context:
                 if word in self.signature[sense] and word not in STOP_WORDS:
                     if self.idf:
-                        scores[i] += math.log(len(self.signature[sense])/sum([1 for sense in self.signature if word in self.signature[sense]]))
+                        # scores[i] += math.log(len(self.signature)/sum([1 for sense in self.signature if word in self.signature[sense]]))
+                        doc_freq = sum(1 for s in self.signature if word in self.signature[s])
+                        idf_value = math.log(len(self.signature) / doc_freq) if doc_freq > 0 else 0
+                        scores[i] += idf_value
                     else:
                         scores[i] += 1
         mfs = senses[scores.index(max(scores))] # get the sense with the highest score 
-
         return mfs
         
 
@@ -164,25 +177,44 @@ class SimplifiedLesk(WSDClassifier):
     def __str__(self):
         return "SimplifiedLesk"
 
+
 def cross_validate(classifier, instances, k=5):
+    """`````
+    in this cross validation, kf is the KFold object, which is used to split the data into k folds.
+    when we set k = 5, we split the data into 5 folds, and we train the model on 4 folds and test on the remaining fold.
+    we repeat this process 5 times, each time we use a different fold as the test fold.
+
+    from the index length, we can see that the data is split into 5 folds, and each fold has a different number of instances.
+
+    the score will be then divided by the number of folds to get the average accuracy over the folds.
+
+    for the commented part, it is used to save the train and test instances for debug, so that we can check the instances used in each fold. --not necessary for the final version
+
+    ``` """
+
+
     kf = KFold(n_splits=k, shuffle=True, random_state=42)
+
     scores = []
-
-    for train_index, test_index in kf.split(instances):
-        train_instances = [instances[i] for i in train_index]
-        test_instances = [instances[i] for i in test_index]
-
-        classifier.train(train_instances)
+    for train_index, test_index in kf.split(instances): # split the data k times
+        train_instances = [instances[i] for i in train_index] # get the train instances
+        test_instances = [instances[i] for i in test_index] # get the test instances
+        classifier.train(train_instances) # train on the train instances
         accuracy = classifier.evaluate(test_instances)
         scores.append(accuracy)
-
+        
+        
     return scores
 
+
 class NaiveBayesWSD(WSDClassifier):
+    from sklearn.feature_extraction.text import CountVectorizer
     def __init__(self):
-        self.model = make_pipeline(CountVectorizer(), MultinomialNB())
+        self.stop_words = list(STOP_WORDS) # we use the stopwords that given by the pr,to suit the CountVectorizer, we need to convert the stopwords to a list from a set dict --- SUN
+        self.model = make_pipeline(CountVectorizer(stop_words=self.stop_words), MultinomialNB())
 
     def train(self, instances):
+    
         texts = [' '.join(i.context) for i in instances]  # Convert list of words to a single string
         labels = [i.sense for i in instances]
         self.model.fit(texts, labels)
@@ -190,6 +222,7 @@ class NaiveBayesWSD(WSDClassifier):
     def predict_sense(self, instance):
         context_str = ' '.join(instance.context)  # Ensure the context is a single string
         return self.model.predict([context_str])[0]
+
 ###############################################################
 
 
@@ -218,75 +251,91 @@ if __name__ == '__main__':
     
     # Displays the sense distributions.
     pass # TODO
+## Task 1: 
 
     senses =[instance.sense for instance in instances]
     senses = pd.Series(senses)
     senses.value_counts().plot(kind='bar')
-    # plt.show()
+    plt.tight_layout()
+    plt.savefig('sense_distribution.png')
+    plt.show()
+    plt.close()
     prettyprint_sense_distribution(instances=instances)
 
     
     # Evaluation of the random baseline on the whole corpus.
-
+## Task2: 
     pass # TODO
     random_baseline = RandomSense()
-    # counter = 0
-    # for instance in instances:
-    #     if instance.sense == random_baseline.predict_sense(instance):
-    #         counter += 1
-    # accuracy = counter / len(instances)
-    # print("Random baseline accuracy by calculating: ", accuracy)
     print(f"Random baseline accuracy: {random_baseline.evaluate(instances)}")
             
     
     # Evaluation of the most frequent sense baseline using different splits of the corpus (with `utils.data_split` or `utils.random_data_split`).
+## Task3: 
     pass # TODO
-    mfs_baseline = MostFrequentSense()
-    train,test= data_split(instances, p=8, n=10)
-    mfs_baseline.train(train)
-    print(f"Most Frequent Sense baseline accuracy: {mfs_baseline.evaluate(test)}")
+    # with random split
+    mfs_baseline_rd = MostFrequentSense()
+    train_rd,test_rd= random_data_split(instances, p=8, n=10)
+    mfs_baseline_rd.train(train_rd)
+    # with fixed split
+    mfs_baseline_fs = MostFrequentSense()
+    train_fs,test_fs= data_split(instances, p=8, n=10)
+    mfs_baseline_fs.train(train_fs)
+    print(f"Most Frequent Sense baseline accuracy with random split technique: {mfs_baseline_rd.evaluate(test_rd)}, with fixed split technique: {mfs_baseline_fs.evaluate(test_fs)}")
+
     
    
     
     # Evaluation of Simplified Lesk (with no fixed window and no IDF values) using different splits of the corpus.
+## Task4: 
     pass # TODO
-    # split 
-    # train,test = random_data_split(instances, p=8, n=10)
-    # if we keep the same train , it will easily be 1 at acc so we many change something
-    # for i in range(10):
-    #     train,test = random_data_split(instances, p=i, n=10)
-    #     clf = SimplifiedLesk()
-    #     clf.train(train)
-    #     print(f"Simplified Lesk Accuracy without specifying Window size and IDF=FALSE:{clf.evaluate(test)}"
+    # with fixed split
     clf = SimplifiedLesk()
     train_clf,test_clf = data_split(instances, p=7, n=10)
     clf.train(train_clf)
-    print(f"Simplified Lesk Accuracy without specifying Window size and IDF=FALSE:{clf.evaluate(test_clf)}")
+    # with random split
+    clf_rd = SimplifiedLesk()
+    train_clf_rd,test_clf_rd = random_data_split(instances, p=7, n=10)
+    clf_rd.train(train_clf_rd)
+    print(f"Simplified Lesk Accuracy without specifying Window size and IDF=FALSE:{clf.evaluate(test_clf)}, with random split technique: {clf_rd.evaluate(test_clf_rd)}")
 
 
 
     
     # Evaluation of Simplified Lesk (with a window of size 10 and no IDF values) using different splits of the corpus.
+##  
     pass # TODO
-
+    # with fixed split
     clf_window= SimplifiedLesk(window_size=10,idf=False)
 
     clf_window.train(train_clf)
-    print(f"Simplified Lesk accuracy with window size fixed at 10 and IDF= FALSE: {clf_window.evaluate(test_clf)}")
+    # with random split
+    clf_window_rd = SimplifiedLesk(window_size=10,idf=False)
+    clf_window_rd.train(train_clf_rd)
+    print(f"Simplified Lesk accuracy with window size fixed at 10 and IDF= FALSE: {clf_window.evaluate(test_clf)}, with random split technique: {clf_window_rd.evaluate(test_clf_rd)}")
     
     # Evaluation of Simplified Lesk (with IDF values and no fixed window) using different splits of the corpus.
+## 
     pass # TODO
+    # with fixed split
     clf_idf = SimplifiedLesk(idf=True)
     clf_idf.train(train_clf)
-    print(f"Simplified Lesk accuracy with IDF=TRUE and no fix window size: {clf_idf.evaluate(test_clf)}")
+    # with random split
+    clf_idf_rd = SimplifiedLesk(idf=True)
+    clf_idf_rd.train(train_clf_rd)
+
+    print(f"Simplified Lesk accuracy with IDF=TRUE and no fix window size: {clf_idf.evaluate(test_clf)}, with random split technique: {clf_idf_rd.evaluate(test_clf_rd)}")
     
     # Cross-validation
-    pass # TODO
+## Task7:  
+    pass # 
     lesk_classifier = SimplifiedLesk(window_size=10)
     lesk_scores = cross_validate(lesk_classifier, instances)
-    print(f'Average accuracy over {len(lesk_scores)} folds: {sum(lesk_scores) / len(lesk_scores)}') 
-    # Naive Bayes classifier
+    print(f'Average accuracy over {len(lesk_scores)} folds: {sum(lesk_scores) / len(lesk_scores)}')  # calculate the average accuracy over the folds 
+    # Naive Bayes classifier 
     pass # TODO
+## Instead of from scratch, we adopte SKLEARN to implement the Naive Bayes classifier, please understand the naviebayes general idea --- SUN
     nb_classifier = NaiveBayesWSD()
     nb_classifier.train(train_clf)
     print(f"Naive Bayes classifier accuracy: {nb_classifier.evaluate(test_clf)}")
+
